6e968c10ee04373a1abbb4d4e9ea710d3d9a7e44
David Chang <davidc@hasoffers.com>
2015-07-26 09:11:53 -0700

    initial base graphql compliant express server

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import schema from './schema';
+import { graphql } from 'graphql';
+import bodyParser from 'body-parser';
+
+let app  = express();
+let PORT = 3000;
+
+// parse POST body as text
+app.use(bodyParser.text({ type: 'application/graphql' }));
+
+app.post('/graphql', (req, res) => {
+  // execute GraphQL!
+  graphql(schema, req.body)
+  .then((result) => {
+    res.send(JSON.stringify(result, null, 2));
+  });
+});
+
+let server = app.listen(PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('GraphQL listening at http://%s:%s', host, port);
+});
