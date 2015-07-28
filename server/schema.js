3ffee92586fdda4386152c590e1740efedc3a393
David Chang <davidc@hasoffers.com>
2015-07-28 12:11:31 -0700

    adding caughtPokemon mutation

--- server/schema.js
+++ server/schema.js
@@ -80,13 +80,20 @@ let schema = new GraphQLSchema({
 
               let collection = db.collection('users');
 
+              // see if the user already exists
               collection.find({ name })
                 .toArray((err, docs) => {
+                  if (err) {
+                    db.close();
+                    return deferred.reject(err);
+                  }
+
                   if (docs.length) {
                     db.close();
                     return deferred.resolve(docs[0]);
                   }
 
+                  // insert the user if they do not exist
                   collection.insert(toCreate, (err, result) => {
                     db.close();
 
@@ -102,6 +109,57 @@ let schema = new GraphQLSchema({
               return deferred.promise;
             });
 
+        }
+      },
+      caughtPokemon: {
+        type: UserType,
+        args: {
+          name: {
+            description: 'The name of the user',
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          pokemon: {
+            description: 'The name of the Pokemon that was caught',
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve: (obj, {name, pokemon}) => {
+          return mongo()
+            .then(db => {
+              let deferred = Q.defer();
+
+              let collection = db.collection('users');
+
+              // find the user
+              collection.find({ name })
+                .toArray((err, docs) => {
+                  if (err || !docs.length) {
+                    db.close();
+                    return deferred.reject(err || 'The user was not found');
+                  }
+
+                  let caught = docs[0].caught;
+                  caught.push(pokemon);
+
+                  // update the user with updated caught array
+                  collection.update({ name }, {
+                    $set : { caught }
+                  }, (err, result) => {
+                    if (err) {
+                      db.close();
+                      return deferred.reject(err);
+                    }
+
+                    deferred.resolve({
+                      name : docs[0].name,
+                      created : docs[0].created,
+                      caught
+                    });
+                  });
+                });
+
+              return deferred.promise;
+            });
         }
       }
     }
